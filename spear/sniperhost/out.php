2eee0a3b193e2f9644cbd9cf4b42b610d42160de
Gem George <gemgeorge4@gmail.com>
2023-01-19 22:42:20 +0530

    path_fix

--- spear/sniperhost/out.php
+++ spear/sniperhost/out.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__,2) . '/db.php');
+require_once(dirname(__FILE__,2) . '/config/db.php');
 
 if(isset($_GET['ht']))	//host text
 	displayHostPlainText($conn,$_GET['ht']);
